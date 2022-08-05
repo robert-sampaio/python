75d663b7a44f814615804d02550b1cf19a4b8b7a
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-08-05 18:27:19 -0300

    Update 1.0
    - Created Desafio001.py.

--- desafios/desafio02.py
+++
@@ -1,4 +0,0 @@
-dia = input('Em que dia você nasceu? ')
-mes = input('Em que mês você nasceu? ')
-ano = input('Em que ano você nasceu? ')
-print('Você nasceu dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
--- desafios/desafio03.py
+++
@@ -1,3 +0,0 @@
-num1 = input('Digite um número: ')
-num2 = input('Digite outro número: ')
-print('A soma entre ' + num1 + ' e ' + num2 + ' é igual a ' + num1 + num2)
